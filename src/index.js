b753efae7cc5122efc2872b068e15820a61bcd73
openunderground <openunderground@gmail.com>
2024-04-01 17:31:34 -0400

    revision

--- src/index.js
+++ src/index.js
@@ -1,214 +1,201 @@
-const REGEX_CONDITION =
-  /\?\((?:(\w+)\.)?(\w+)\s*(==|<|>)\s*["']?([^"'\s]+)["']?\)/;
-const REGEX_PROPS = /\$(?:(\w+)\.)?(\w+)\s*=\s*("[^"]+"|'[^']+'|\b\w+\b|\d+)/;
-const REGEX_EMOTION = /\{\{.+?\}\}/g;
+const RegEx = {
+  EXTRACT_PROPS: /\$(?:(\w+)\.)?(\w+)\s*=\s*("[^"]+"|'[^']+'|\b\w+\b|\d+)/,
+  EXTRACT_CONDITION: /\?\((?:(\w+)\.)?(\w+)\s*(==|<|>)\s*["']?([^"'\s]+)["']?\)/,
+  EXTRACT_EMOTION: /\{\{([\w, +]+)\}\}/,
+  IS_LINK: /^!*\[\[.+?\]\]!*$/g,
+  DOUBLE_SQUARE_BRACKETS: /^!*\[\[|\]\]!*$/g,
+}
 
-const decodeHtmlEntities = (text) => {
-  return text.replace(/&lt;/g, "<").replace(/&gt;/g, ">");
-};
+class Parser {
+  twstory;
 
-const parseValueAsLuaObject = (value) => {
-  const valueType = typeof value;
-  if (value === null) return null;
-  if (Array.isArray(value)) {
-    const elements = value.map(parseValueAsLuaObject).join(", ");
-    return `{${elements}}`;
+  constructor(twstory) {
+    this.twstory = twstory;
+    this.convertPassage = this.convertPassage.bind(this);
+    this.parseResponse = this.parseResponse.bind(this);
+
+    console.log(this.toJSON());
   }
 
-  switch (valueType) {
-    case "number":
-    case "boolean":
-      return value.toString();
-    case "string":
-      return JSON.stringify(value);
-    case "object": {
-      const properties = Object.entries(value)
-        .map(([key, val]) => `${key} = ${parseValueAsLuaObject(val)}`)
-        .join(", ");
-      return `{${properties}}`;
+  toJSON() {
+    console.log("Converting to Story //");
+    const { name, startnode: startNodeId } = this.attrsToObject(this.twstory.attributes)
+    const passagesNodes = this.twstory.getElementsByTagName("tw-passagedata");
+    const convertedPassages = [...passagesNodes].map(this.convertPassage);
+    const startNode = convertedPassages.find((passage) =>
+      passage.pid == startNodeId
+    )
+
+    const passages = {}
+    convertedPassages.forEach((row) => {
+      passages[row.name] = { ...row, name: undefined, pid: undefined };
+    });
+
+    return {
+      name,
+      passages,
+      startNode: startNode.name,
+    };
+  }
+
+  convertPassage(passage) {
+    const converted = passage.innerHTML.split("\n")
+      .map(line => this.decodeHtmlEntities(line).trim())
+      .filter(Boolean)
+      .reduce((acc, lineRaw) => {
+
+        // Props are expected to be on a line by themselves
+        const propsMatch = lineRaw.match(RegEx.EXTRACT_PROPS);
+        if (propsMatch) {
+          acc.props = this.getProp(propsMatch, acc.props)
+          return acc;
+        }
+
+        const entry = {}
+        let workingLine = lineRaw;
+
+        // Dialogue and Response lines may have conditional and emotions
+        // conditional and emotions must be at the start of the line
+        const condition = this.getCondition(workingLine);
+        if (condition) entry.condition = condition;
+        workingLine = workingLine.replace(RegEx.EXTRACT_CONDITION, "");
+
+        const emotions = workingLine.match(RegEx.EXTRACT_EMOTION);
+        if (emotions) entry.emotions = emotions[1].split(',').map(t => t.trim()).filter(Boolean);
+        workingLine = workingLine.replace(RegEx.EXTRACT_EMOTION, "");
+
+        const linkMatch = workingLine.match(RegEx.IS_LINK);
+        if (linkMatch) {
+          const linkContent = workingLine.replace(RegEx.DOUBLE_SQUARE_BRACKETS, "").split("|");
+          if (linkContent && linkContent.length == 1) {
+            entry.text = null;
+            entry.link = linkContent[0];
+            acc.redirects.push(entry);
+            return acc;
+          }
+          if (linkContent && linkContent.length == 2) {
+            const [text, link] = linkContent;
+            entry.text = linkContent[0];
+            entry.link = linkContent[1];
+            acc.responses.push(entry);
+            return acc;
+          }
+          // invalid, skip
+          return acc;
+        }
+
+        entry.text = workingLine;
+        acc.lines.push(entry);
+        return acc;
+      }, {
+        lines: [],
+        responses: [],
+        redirects: [],
+        props: {}
+      })
+
+    const { name, pid, tags } = this.attrsToObject(passage.attributes)
+    return {
+      ...linesConverted,
+      tags: tags.split(" ").filter(Boolean),
+      name,
+      pid,
     }
-    default:
+  }
+
+  getProp(matchResult, props) {
+    const [_fullMatch, groupName, propName, value] = matchResult;
+
+    let typedValue
+    try {
+      typedValue = JSON.parse(value);
+    } catch (error) {
+      typedValue = value;
+    }
+
+    if (groupName) {
+      props[groupName] = props[groupName] || {};
+      props[groupName][propName] = typedValue;
+    }
+    else {
+      props[propName] = typedValue;
+    }
+    return props;
+  }
+
+  getCondition(text) {
+    const match = text.match(RegEx.EXTRACT_CONDITION);
+    if (!match) return null;
+
+    let [_, group, prop, comparator, value] = match;
+
+    switch (comparator) {
+      case "==": comparator = "eq"; break;
+      case ">": comparator = "gt"; break;
+      case "<": comparator = "lt"; break;
+      case ">=": comparator = "gte"; break;
+      case "<=": comparator = "lte"; break;
+    }
+
+    try {
+      value = JSON.parse(value);
+    } catch (error) {}
+
+    return { group, prop, comparator, value };
+  }
+
+  toLuaTableString() {
+
+    const parseLua = (value) => {
+      if (value === null) return null;
+
+      if (Array.isArray(value)) {
+        const elements = value.map(parseLua).join(", ");
+        return `{${elements}}`;
+      }
+
+      const valueType = typeof value;
+
+      if (valueType == "number" || valueType == "number")
+        return value.toString();
+
+      if (valueType == "string")
+        return JSON.stringify(value);
+
+      if (valueType == "object") {
+        const properties = Object.entries(value)
+          .map(([key, val]) => `${key} = ${parseLua(val)}`)
+          .join(", ");
+        return `{${properties}}`;
+      }
+
       throw new Error(`Unsupported data type: ${valueType}`);
-  }
-};
+    };
 
-const convertToLuaScript = (data) => {
-  return `return ${parseValueAsLuaObject(data)}`;
-};
-
-const getCondition = (text) => {
-  const match = text.match(REGEX_CONDITION);
-  if (!match) return;
-  let [_, category, varName, comparator, value] = match;
-
-  switch (comparator) {
-    case "==":
-      comparator = "eq";
-      break;
-    case ">":
-      comparator = "gt";
-      // value = parseFloat(value);
-      break;
-    case "<":
-      comparator = "lt";
-      // value = parseFloat(value);
-      break;
-  }
-  try {
-    value = JSON.parse(value);
-  } catch (error) {}
-  return { varName, category: category ?? "checks", comparator, value };
-};
-
-const parseResponse = ({ unparsedText, emotion }) => {
-  const safeLink = unparsedText.replace(/^!*\[\[|\]\]!*$/g, "");
-  const splitLink = safeLink.split("|");
-  const toReturn = {};
-  if (splitLink.length == 1) toReturn.link = splitLink[0];
-
-  const condition = getCondition(splitLink[0]);
-  if (condition) toReturn.condition = condition;
-
-  if (splitLink.length == 2) {
-    toReturn.link = splitLink[1];
-    if (!condition) {
-      toReturn.text = splitLink[0];
-    }
-  } else if (splitLink.length == 3) {
-    toReturn.link = splitLink[2];
-    toReturn.text = splitLink[1];
+    const data = this.toJSON();
+    return `return ${parseLua(data)}`;
   }
 
-  if (toReturn.text?.startsWith("---")) {
-    toReturn.isEnd = true;
-    toReturn.text = toReturn.text.replace(/^---/, "");
-    if (!toReturn.text) delete toReturn.text;
-  }
-  if (unparsedText.startsWith("!") && unparsedText.endsWith("!")) {
-    toReturn.isUrgent = true;
-  }
-  if (emotion) toReturn.emotion = emotion;
-
-  return toReturn;
-};
-
-const extractResponsesFromText = (texts) => {
-  const responses = [];
-
-  for (let i = texts.length - 1; i >= 0; i--) {
-    const currentString = texts[i];
-    const responseMatches = currentString.match(/!*\[\[.+?\]\]!*/g);
-    const emotionMatches = currentString.match(REGEX_EMOTION);
-
-    if (responseMatches) {
-      const newResponse = { unparsedText: responseMatches[0] };
-      if (emotionMatches) {
-        newResponse.emotion = emotionMatches[0].replace(/^\{\{|\}\}$/g, "");
-      }
-      responses.unshift(newResponse);
-      texts.splice(i, 1);
-    }
+  attrsToObject(node) {
+    return Object
+      .entries(node)
+      .reduce((acc, [key, value]) => ({
+        ...acc,
+        [value.name]: value.value
+      }), {})
   }
 
-  if (responses.length == 0) return;
-
-  return responses.map(parseResponse).sort(response => response.isUrgent ? 1 : 0);
-};
-
-const extractPropsFromText = (texts) => {
-  const props = [];
-
-  for (let i = texts.length - 1; i >= 0; i--) {
-    const currentString = texts[i];
-    const match = currentString.match(REGEX_PROPS);
-
-    if (match) {
-      const [_, category, varName, value] = match;
-      const prop = { varName, category: category ?? "checks" };
-      try {
-        prop.value = JSON.parse(value);
-      } catch (error) {
-        prop.value = value;
-      }
-
-      props.push(prop);
-      texts.splice(i, 1);
-    }
+  decodeHTMLEntities(text) {
+    var textArea = document.createElement('textarea');
+    textArea.innerHTML = text;
+    return textArea.value;
   }
-
-  return Object.keys(props).length > 0 ? props : null;
-};
-
-const cleanLinesArray = (texts) => {
-  return texts
-    .map((text) => decodeHtmlEntities(text).trim())
-    .filter((text) => !!text);
-};
-
-const parseLine = (line) => {
-  const toReturnLine = {
-    text: line.replace(REGEX_CONDITION, "").replace(REGEX_EMOTION, ""),
-  };
-  const condition = getCondition(line);
-  const emotion = line.match(REGEX_EMOTION);
-
-  if (condition) toReturnLine.condition = condition;
-  if (emotion) toReturnLine.emotion = emotion[0].replace(/^\{\{|\}\}$/g, "");
-
-  return toReturnLine;
-};
-
-const convertPassage = (passage) => {
-  const lines = cleanLinesArray(passage.innerHTML.split("\n"));
-  const dict = {};
-
-  const responses = extractResponsesFromText(lines);
-  if (responses) {
-    dict.responses = responses.filter((response) => !!response.text);
-    dict.redirects = responses.filter((response) => !response.text);
-    if (!dict.responses.length) delete dict.responses;
-    if (!dict.redirects.length) delete dict.redirects;
-  }
-
-  const props = extractPropsFromText(lines);
-  if (props) dict.props = props;
-
-  ["name", "tags", "pid"].forEach((attr) => {
-    const value = passage.attributes[attr].value;
-    if (value) dict[attr] = value;
-  });
-
-  if (dict.tags) dict.tags = dict.tags.split(" ");
-  dict.lines = cleanLinesArray(lines).map(parseLine);
-
-  return dict;
-};
-
-const convertStory = (story) => {
-  const passages = story.getElementsByTagName("tw-passagedata");
-  const convertedPassages = Array.prototype.slice
-    .call(passages)
-    .map(convertPassage);
-
-  const dict = {
-    passages: {},
-    name: story.attributes.name.value,
-    start_node_name: convertedPassages.find(
-      (passage) => passage.pid == story.attributes.startnode.value
-    ).name,
-  };
-
-  convertedPassages.forEach((row) => {
-    dict.passages[row.name] = { ...row, name: undefined, pid: undefined };
-  });
-
-  return JSON.parse(JSON.stringify(dict));
-};
+}
 
 const parseTwineToLua = () => {
-  const storyData = document.getElementsByTagName("tw-storydata")[0];
-  const response = convertToLuaScript(convertStory(storyData));
-  document.getElementById("output").innerHTML = response;
+  const twStorydata = document.getElementsByTagName("tw-storydata")[0];
+  const parser = new Parser(twStorydata);
+  document.getElementById("output").innerHTML = parser.toLuaTableString()
 };
 
 window.parseTwineToLua = parseTwineToLua;
